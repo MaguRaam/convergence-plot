6c14586ce9ba2b8822772c3aba05d66a6c2a4907
MaguRaam <rp361@snu.edu.in>
2020-10-05 21:09:12 +0530

    added small change

--- convergence.cc
+++ convergence.cc
@@ -13,30 +13,26 @@
 namespace plt = matplotlibcpp;
 
 //convergence rate
-template<typename T>
 inline
-auto rate_of_convergence(const T& e1, const T& e2)
+double rate_of_convergence(size_t N1, double e1, size_t N2, double e2)
 {
-	return (log(e1) - log(e2)) / log(2); 
+	return (log(e1) - log(e2)) / (log(N2) - log(N1)); 
 }
-
 
 
 int main()
 {
-	std::cout.flags( std::ios::dec | std::ios::scientific ) ; 
-	std::cout.precision(6) ;
+	std::cout.flags( std::ios::dec | std::ios::scientific ); 
+	std::cout.precision(6);
 
 	//L2 and Linfty error vs N
-	std::vector<int> 		N{32, 64, 128, 256};
-	std::vector<double>		linfty{2.655393e-03, 2.012193e-04, 8.542100e-06, 1.995890e-07};
-	std::vector<double>		l2{1.292740e-03, 9.325463e-05, 3.731197e-06, 1.273324e-07};
+	std::vector<int> 		N{32,64, 96, 128, 192};
+	std::vector<double>		linfty{2.655393e-03, 2.012193e-04, 3.481511e-05, 8.542100e-06, 1.461659e-06};
+	std::vector<double>		l2{1.292740e-03, 9.325463e-05, 1.575852e-05, 3.731197e-06, 8.588976e-07};
 
-	for (int i = 1; i < N.size(); ++i){
-		std::cout<<N[i]<<"\t"<<rate_of_convergence(linfty[i-1],linfty[i])<<"\t"<<
-							   rate_of_convergence(l2[i-1],l2[i])<<std::endl;
-	}
-
+	for (int i = 1; i < N.size(); ++i) 
+		std::cout<<N[i]<<"\t"<<rate_of_convergence(N[i-1], linfty[i-1], N[i], linfty[i])<<"\t"<<
+							   rate_of_convergence(N[i-1], l2[i-1], 	N[i], l2[i])<<std::endl;
 
 
 	//Convergence plot:
